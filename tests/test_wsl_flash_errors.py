e4a3ced941d91c0546012da7712e7806ead8d39f
flouz15 <1284641206@qq.com>
2026-06-02 15:30:49 +0800

    fix wsl flash Abnormal popup bug

--- tests/test_wsl_flash_errors.py
+++ tests/test_wsl_flash_errors.py
@@ -5,7 +5,7 @@ import sys
 
 sys.path.insert(0, str(Path(__file__).resolve().parents[1]))
 
-from seeed_jetson_develop.wsl_flash import _looks_like_nfs_mount_failure
+from seeed_jetson_develop.wsl_flash import _looks_like_nfs_mount_failure, _parse_sc_state
 
 
 def test_detects_nfs_mount_failure_message():
@@ -25,3 +25,12 @@ Waiting for target to boot-up...
 Waiting for target to boot-up...
 """
     assert not _looks_like_nfs_mount_failure(text)
+
+
+def test_parse_sc_service_state():
+    text = """
+SERVICE_NAME: ShellHWDetection
+        TYPE               : 20  WIN32_SHARE_PROCESS
+        STATE              : 4  RUNNING
+"""
+    assert _parse_sc_state(text) == "RUNNING"
